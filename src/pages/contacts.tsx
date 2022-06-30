79e5a2c7b14e55b61f34fe1eec35d54f049dd3a1
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-06-30 22:24:11 +0300

    add pages

---
+++ src/pages/contacts.tsx
@@ -0,0 +1,34 @@
+import { Layout } from "../components/Layout/Layout"
+import { Container } from "../components/Container/Container"
+import { Contact } from "../components/Contact/Contact"
+import styles from './../styles/Contacts.module.css'
+
+export default function About() {
+  return (
+    <Layout>
+      <div className={styles.Contacts}>
+      <Container>
+        <div className={styles.Contacts__List}>
+          <Contact
+            socialMedia='Telegram'
+            link='https://telegram.me/+380508801537'
+            label='Andrey Shvedov'
+          />
+
+          <Contact
+            socialMedia='Google'
+            link='mailto:umr.translate.group@gmail.com'
+            label='UMR Translate Group'
+          />
+
+          <Contact
+            socialMedia='Instagram'
+            link='https://instagram.com/_andrey_shvedov_'
+            label='Andrey Shvedov'
+          />
+        </div>
+      </Container>
+    </div>
+    </Layout>
+  )
+}
