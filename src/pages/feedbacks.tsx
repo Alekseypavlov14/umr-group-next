159f629457bd31a7b52731aa243abb0a7b4a229c
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-07-05 20:50:35 +0300

    add new page (feedbacks)

---
+++ src/pages/feedbacks.tsx
@@ -0,0 +1,33 @@
+import { Button } from '../components/Button/Button'
+import { Container } from '../components/Container/Container'
+import { Feedback } from '../components/Feedback/Feedback'
+import { FeedbackPanel } from '../components/FeedbackPanel/FeedbackPanel'
+import { FeedbackType } from '../types/Feedback.type'
+import styles from './../styles/pages/Feedbacks.module.css'
+
+export default function Feedbacks({ feedbacks }) {
+  return (
+    <div className={styles.Feedbacks}>
+      <Container>
+        <div className={styles.FeedbackContainer}>
+          {feedbacks.map((feedback, index) => (
+            <Feedback feedback={feedback} key={index} />
+          ))}
+        </div>
+      </Container>     
+
+      <FeedbackPanel /> 
+    </div>
+  )
+}
+
+export async function getServerSideProps() {
+  const response = await fetch('https://umr-group.herokuapp.com/feedbacks')
+  const data = await response.json()
+
+  return {
+    props: {
+      feedbacks: data.feedbacks as FeedbackType[]
+    }
+  }
+}
