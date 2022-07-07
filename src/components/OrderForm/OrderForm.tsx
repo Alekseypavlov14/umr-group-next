c27516d7c110f1820e34f3c1bc7ac5fd4fb8f585
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-07-07 19:00:53 +0300

    add select component

--- src/components/OrderForm/OrderForm.tsx
+++ src/components/OrderForm/OrderForm.tsx
@@ -1,10 +1,48 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { Button } from '../Button/Button'
+import { AdditiveInput } from '../AdditiveInput/AdditiveInput'
+import { useDispatch, useSelector } from 'react-redux'
+import { Order } from '../../types/Order.type'
+import { OrderAdditive } from '../../types/OrderAdditive.type'
+import { countPrice } from '../../business/countOrderPrice/CountPrice'
+import { changeOrder, orderSelector, updateAdditive, updateDate } from '../../features/order/orderSlice'
+import { Select } from './../Select/Select'
 import styles from './OrderForm.module.css'
 
-interface OrderFormProps {}
+interface OrderFormProps {
+  orders: Order[]
+}
 
-const OrderForm: FC<OrderFormProps> = () => {
+const OrderForm: FC<OrderFormProps> = ({ orders }) => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(changeOrder(orders[0]))
+  }, [])
+
+  const order = useSelector(orderSelector)
+
+  const convertOrderToOption = (order: Order) => ({
+    label: order.label,
+    value: order.name
+  })
+
+  function getOrderByName(name: string): Order {
+    return orders.find(order => order.name === name)
+  }
+
+  function getInputDateFormatted(time: number) {
+    const now = new Date(time)
+
+    let day = now.getDate().toString()
+    if (day.length === 1) day = '0' + day
+
+    let month = (now.getMonth() + 1).toString()
+    if (month.length === 1) month = '0' + month
+
+    return `${now.getFullYear()}-${month}-${day}`
+  }
+  
   return (
     <div className={styles.OrderForm}>
       <div className={styles.OrderHeadline}>
@@ -15,48 +53,48 @@ const OrderForm: FC<OrderFormProps> = () => {
           <div className={styles.Description}>
             Обрати послугу:
           </div>
-          {/* <Select
-            defaultValue={convertServiceToOption(service)}
-            options={services.map(convertServiceToOption)}
-            className={styles.Select}
+          <Select
+            options={orders.map(convertOrderToOption)}
             onChange={(e) => {
               if (!e) return
-              uncheckAllAdditives()             
-              const service = getServiceByName(e.value)
-              setService(service)
-              setCurrentPrice(service.startPrice + 20)
-              setTime(Date.now())
+              dispatch(changeOrder(getOrderByName(e.value)))
             }} 
-          /> */}
+          />
         </div>
 
         <div className={styles.FormSection}>
           <div className={styles.Description}>Опції:</div>
-          {/* {service.additives.map((additive, index) => (
+          {order.additives.map((additive, index) => (
             <AdditiveInput 
-              key={index}
+              key={additive.name}
               additive={additive}
               onChange={(e) => {
-                if (e.target.checked) return setCurrentPrice(price => price + additive.price)
-                else return setCurrentPrice(price => price - additive.price)
+                dispatch(updateAdditive({
+                  name: additive.name,
+                  label: additive.label,
+                  price: additive.price,
+                  isChecked: e.target.checked
+                }))
               }}
             />
-          ))} */}
+          ))}
         </div>
 
         <div className={styles.FormSection}>
           <div className={styles.Description}>
             Оберіть дату:
           </div>
-          {/* <input 
+          <input 
             className={styles.Input}
-            onChange={(e) => setTime(new Date(e.target.value).getTime())}
-            onFocus={(e) => removeInvalidClass(e)}
-            min={getInputDateFormat(Date.now())}
-            value={getInputDateFormat(time)}
+            onChange={(e) => {
+              dispatch(updateDate(new Date(e.target.value).getTime()))
+            }}
+            onFocus={(e) => {}}
+            min={getInputDateFormatted(Date.now())}
+            value={getInputDateFormatted(order.date)}
             id='date'
             type='date'
-          /> */}
+          />
         </div>
 
         <div className={styles.FormSection}>
@@ -66,22 +104,20 @@ const OrderForm: FC<OrderFormProps> = () => {
           <div className={styles.LetterTip}>
             * Залишіть контакти, щоб ми могли з Вами зв'язатися
           </div>
-          {/* <textarea 
+          <textarea 
             id='letter'
             wrap='soft'
             minLength={30}
             maxLength={500}
             className={styles.Input}
-            onChange={(e) => setLetter(e.target.value)}
-            onFocus={(e) => removeInvalidClass(e)}
-          /> */}
+            onChange={(e) => {}}
+            onFocus={(e) => {}}
+          />
         </div>
 
         <div className={styles.FormSection}>
           <div className={styles.CurrentPrice}>
-            Вартість: 
-            {/* <span>{currentPrice}</span>  */}
-            лей за годину
+            Вартість: <span>{countPrice(order)}</span> лей за годину
           </div>
           <div className={styles.OrderButton}>
             <Button
---
+++ src/components/Select/Select.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react'
+import ReactSelect from 'react-select'
+import styles from './Select.module.css'
+
+interface Option {
+  value: string
+  label: string
+}
+
+interface SelectProps {
+  options: Option[]
+  onChange: (e: Option) => void
+}
+
+const Select: FC<SelectProps> = ({
+  options,
+  onChange
+}) => {
+  return (
+    <ReactSelect
+      options={options}
+      defaultValue={options[0]}
+      className={styles.Select}
+      onChange={onChange}
+    />
+  )
+}
+
+export { Select }
