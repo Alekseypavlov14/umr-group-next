1f6bc5347f7d56be40126eb65b7446c36aa011d0
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-07-07 18:09:57 +0300

    add order type

---
+++ src/types/Order.type.ts
@@ -0,0 +1,10 @@
+import { OrderAdditive } from "./OrderAdditive.type"
+
+export interface Order {
+  name: string
+  label: string
+  startPrice: number
+  additives: OrderAdditive[]
+  date: number,
+  hour: number
+}
